6798dc5bf5eb93e9aeb8f1b9afbe98742d395b2e
Mohamed Awad <awadmohamed233@gmail.com>
2019-03-08 22:37:32 +0200

    fix bugs of samman on github

---
+++ views/Users.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/users.css">
+</head>
+<body>
+
+iv class="tab">
+		<a href="Go to home page.php ">Home</a>
+		<a href="go to products page.php">Products</a>
+		<a href="go to users page.php">Users</a>
+		<a href="go to orders.php">Manual Order</a>
+		<a href="go to checks.php">Checks</a>
+
+		<img id="userImg" src="imgs/user.png" width="40" height="40"/>
+		<label name="UserName">Admin</label>
+	</div>
+
+
+<table>
+	<h1>All Users</h1>
+	<tr><td width="800px"><a href="go to add user.php" align="right">add user</a></td></tr>
+</table>
+              echo '<table border=1 class="usersTable" >';
+              echo "<tr>
+                <td>Name</td>
+                <td>Room</td>
+                <td>Image</td>                 
+                <td>Action</td>
+                </tr>";
+			echo "<tr>
+                <td>Name</td>
+                <td>Room</td>
+                <td>Image</td>                 
+                <td>Action</td>
+                </tr>";
+			echo "<tr>
+                <td>Name</td>
+                <td>Room</td>
+                <td>Image</td>                 
+                <td>Action</td>
+                </tr>";
+			echo "<tr>
+                <td>Name</td>
+                <td>Room</td>
+                <td>Image</td>                 
+                <td>Action</td>
+                </tr>";
+                
+</body>
+</html>
