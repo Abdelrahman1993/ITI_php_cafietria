300a4b468800d8fc81b228c4a427e37396a02193
HeshamSayed <hesham.sayed636@gmail.com>
2019-03-09 18:52:49 +0200

    creating login page , checks if the user admin or not , change the connection of the db to PDO

---
+++ Cafeteria/init.php
@@ -0,0 +1,7 @@
+<?php
+
+include 'dbConnection.php';
+
+$tpl = 'includes/template/' ; // template directory
+$css = 'Layout/css/'; // css directory
+$js  =  'Layout/js/'; // js directory
--- dbConnection.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$conn = mysqli_connect("localhost",'root','',"cafeteria");
-                if($conn){
-                    echo "connect";
-                }
-?>
