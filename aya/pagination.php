c208cbb79a711c7fc7cbbd6413c89cefd33d2130
aya abdalla <ayalaban2@gmail.com>
2019-03-13 00:21:35 +0200

    Pagination to Products

---
+++ aya/pagination.php
@@ -0,0 +1,52 @@
+
+<head>
+    <title>Cafateria</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+</head>
+<?php
+
+if (isset($_GET['pageno'])) {
+    $pageno = $_GET['pageno'];
+} else {
+    $pageno = 1;
+}
+$no_of_records_per_page = 3;
+$offset = ($pageno-1) * $no_of_records_per_page;
+
+$conn=mysqli_connect("localhost","root","","cafeteria");
+if (mysqli_connect_errno()){
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    die();
+}
+
+$total_pages_sql = "SELECT COUNT(*) FROM Products";
+$result = mysqli_query($conn,$total_pages_sql);
+$total_rows = mysqli_fetch_array($result)[0];
+$total_pages = ceil($total_rows / $no_of_records_per_page);
+
+$sql = "SELECT * FROM Products LIMIT $offset, $no_of_records_per_page";
+$res_data = mysqli_query($conn,$sql);
+while($row = mysqli_fetch_array($res_data)){
+    echo '<div class="col-lg-3">';
+    echo '<img alt="'.$row['price'].'" name="'.$row['name'].'" class="imgSize" id="'.$row['id'].'" src="'.$row['img_path'].'" /><br>';
+    echo  '<strong class="productName">'.$row['name'].'</strong><br>';
+    echo '<strong> price:</strong><strong class="price">'.$row['price'].'</strong><strong> EGP</strong>';
+    echo ' </div>';
+}
+mysqli_close($conn);
+?>
+<ul class="pagination">
+<li><a href="?pageno=1">First</a></li>
+<li class="<?php if($pageno <= 1){ echo 'disabled'; } ?>">
+    <a href="<?php if($pageno <= 1){ echo '#'; } else { echo "?pageno=".($pageno - 1); } ?>">Prev</a>
+</li>
+<li class="<?php if($pageno >= $total_pages){ echo 'disabled'; } ?>">
+    <a href="<?php if($pageno >= $total_pages){ echo '#'; } else { echo "?pageno=".($pageno + 1); } ?>">Next</a>
+</li>
+<li><a href="?pageno=<?php echo $total_pages; ?>">Last</a></li>
+</ul>
