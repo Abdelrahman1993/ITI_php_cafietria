664fd1010c1e3152c91aa9410d421a8c0b8e8a2b
Mohamed Awad <awadmohamed233@gmail.com>
2019-03-17 14:10:17 +0200

    release 5.9

--- sendmail/send_email.php
+++ sendmail/send_email.php
@@ -2,11 +2,26 @@
   session_start();
   //print_r($_SESSION);
   $noHeader = '';
-  include 'init.php';
+//  include 'init.php';
 
   echo "0000<br>";
   if($_SERVER['REQUEST_METHOD'] == 'POST')
   {
+    $dsn    = 'mysql:host=sql2.freemysqlhosting.net;dbname=sql2283363';
+    $user   = 'sql2283363';
+    $pass   = 'tF7%wG1*';
+    $option = array(
+        PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8', );
+    try {
+        $con = new PDO($dsn,$user,$pass ,$option);
+        $con ->setAttribute(PDO::ATTR_ERRMODE , PDO::ERRMODE_EXCEPTION);
+    }
+    catch(PDOException $e)
+    {
+        echo 'faild to connect' . $e->getMessage();
+    }
+
+
     echo "1111<br>";
     $username = $_POST['user'];
     var_dump($con);
