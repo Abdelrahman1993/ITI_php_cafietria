f99a47569d9c32185b266dd0db1fa7a719ba107e
Mohamed Awad <awadmohamed233@gmail.com>
2019-03-17 16:16:11 +0200

    use php mailer v7

--- sendmail/send_email.php
+++ sendmail/send_email.php
@@ -1,6 +1,9 @@
 <?php
   session_start();
-  use \PHPMailer\Exception;
+  use PHPMailer\PHPMailer\PHPMailer;
+  use PHPMailer\PHPMailer\Exception;
+
+
   include 'PHPMailer/src/Exception.php';
   include 'PHPMailer/src/PHPMailer.php';
   include 'PHPMailer/src/SMTP.php';
