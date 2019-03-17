5dbc53a6807b5aa5ee9cdd25358f1055532a6b8f
Mohamed Awad <awadmohamed233@gmail.com>
2019-03-17 13:34:33 +0200

    release 5.2

--- sendmail/send_email.php
+++ sendmail/send_email.php
@@ -2,17 +2,18 @@
   session_start();
   $noHeader = '';
   include 'init.php';
+  echo "1111<br>";
   if(isset($_POST['submit']))
   {
     $user_email = $_POST['user'];
     $user_new_pass = $POST['new_pass'];
     $stmt_1 = $con->prepare("SELECT * FROM User where email= ?");
     $stmt_1->execute(array($user_email));
-
+    echo "2222<br>";
     $val_code = "0123456789zxcvbnmqwertyuioplkjhgfdsa";
     $_POST['shuffled'] = str_shuffle($val_code);
     $_POST['shuffled'] = substr($_POST['shuffled'], 0, 8);
-
+    echo "3333<br>";
     $to = $user_email;
     $subject = "new password";
     $txt = "Your new password is : ".password_hash($_POST['shuffled']);
@@ -22,7 +23,8 @@
           'MIME-Version: 1.0' . "\r\n" .
           'Content-type: text/html; charset=utf-8';
     $m=mail($to,$subject,$txt,$headers);
-    echo "hiiiiiiii";
+    echo "3333<br>";
+    echo "444";
     if($m)
     {
       echo'Check your inbox in mail';
